c7777322c9e5db52fe290d2ada530f0096ba756d
Denis Latyshev <d.d.latyshev@yandex.ru>
2024-03-13 14:18:57 +0300

    feat: добавить конфигурацию базы данных PostgresSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'nest_student',
+      database: 'nest_project',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
